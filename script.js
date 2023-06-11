b7e2b0bfb87c8b160372f5cd9aebe573bdefb266
Aditi Tiwari <115400068+Aditiwari7@users.noreply.github.com>
2023-06-11 18:16:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // your code here
+function update(){
+	 var name = document.getElementById("name").value;
+      var year = document.getElementById("year").value;
+
+      var urlString = "https://localhost:8080/";
+      
+      if (name) {
+        urlString += "?name=" + encodeURIComponent(name);
+      }
+      
+      if (year) {
+        urlString += (name ? "&" : "?") + "year=" + encodeURIComponent(year);
+      }
+
+      document.getElementById("url").textContent = urlString;
+})
